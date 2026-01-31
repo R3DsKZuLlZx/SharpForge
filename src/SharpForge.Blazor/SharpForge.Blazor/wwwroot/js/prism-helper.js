7dac69a33cb8097901c8283255855561717c0fb1
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-01-31 17:21:52 +0000

    Add code highlighting.

---
+++ src/SharpForge.Blazor/SharpForge.Blazor/wwwroot/js/prism-helper.js
@@ -0,0 +1,13 @@
+﻿// Prism.js syntax highlighting initialization
+export function highlightAll() {
+    if (typeof Prism !== 'undefined') {
+        Prism.highlightAll();
+    }
+}
+
+export function highlightElement(element) {
+    if (typeof Prism !== 'undefined' && element) {
+        Prism.highlightElement(element);
+    }
+}
+
