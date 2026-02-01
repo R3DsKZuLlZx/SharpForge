ad2a42e019ab1cdb5d0153df660bd170b3a3d976
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-02-01 19:41:56 +0000

    Remove old projects.

--- src/SharpForge.Blazor/SharpForge.Blazor.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SharpForge.Blazor.Client.Services;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddScoped<ThemeService>();
-builder.Services.AddScoped<CourseProgressService>();
-
-await builder.Build().RunAsync();
