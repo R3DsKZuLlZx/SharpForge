c793f6c15c33912456115973f731b45cda529ada
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-02-01 16:46:21 +0000

    Upate course progression.

--- src/SharpForge.Blazor/SharpForge.Blazor.Client/Program.cs
+++ src/SharpForge.Blazor/SharpForge.Blazor.Client/Program.cs
@@ -4,5 +4,6 @@ using SharpForge.Blazor.Client.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped<ThemeService>();
+builder.Services.AddScoped<CourseProgressService>();
 
 await builder.Build().RunAsync();
