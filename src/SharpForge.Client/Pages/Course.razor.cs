11772a0507530603aaa54573f021617a60c67bf9
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-04-06 23:23:53 +0100

    Use code behind for pages.

---
+++ src/SharpForge.Client/Pages/Course.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using SharpForge.Client.Models;
+using SharpForge.Client.Services;
+
+namespace SharpForge.Client.Pages;
+
+public partial class Course
+{
+    [Inject]
+    public required MarkdownService MarkdownService { get; set; }
+    
+    [Parameter]
+    public string Slug { get; set; } = "";
+
+    private CourseDetail? _course;
+
+    protected override void OnParametersSet()
+    {
+        _course = MarkdownService.GetCourse(Slug);
+    }
+}
---
+++ src/SharpForge.Client/Pages/Training.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using SharpForge.Client.Models;
+using SharpForge.Client.Services;
+
+namespace SharpForge.Client.Pages;
+
+public partial class Training
+{
+    [Inject]
+    public required MarkdownService MarkdownService { get; set; }
+    
+    private string _selectedFilter = "All";
+    private IReadOnlyList<CourseDetail> _courses = [];
+
+    protected override void OnInitialized()
+    {
+        _courses = MarkdownService.GetAllCourses();
+    }
+
+    private IEnumerable<CourseDetail> FilteredCourses => _selectedFilter == "All"
+        ? _courses
+        : _courses.Where(c => c.Level == _selectedFilter);
+
+    private void SetFilter(string filter)
+    {
+        _selectedFilter = filter;
+    }
+}
