103cffca059033d02299f8ec7ecf92345ad779e3
Chris Johns <christopher.johns@howdeninsurance.co.uk>
2026-04-18 23:31:06 +0100

    Extract icons.

---
+++ src/SharpForge.Client/Components/Icon.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SharpForge.Client.Components;
+
+public partial class Icon
+{
+    [Parameter, EditorRequired]
+    public string Name { get; set; } = string.Empty;
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    [Parameter]
+    public int Size { get; set; } = 24;
+}
+
